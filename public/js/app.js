5e3cacab2a595eb5fbb2d50eaae1344cf6b4ee6d
Ibrahim Kundlik <ibrahimkundlik@gmail.com>
2021-02-12 22:18:19 +0530

    Updated resources as per new layout

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,65 @@
 const form = document.querySelector("form");
 const input = document.querySelector("#address");
-const weather = document.querySelector(".weather");
+const weatherErr = document.querySelector(".weather-err");
+const search = document.querySelector(".search-logo");
+const header = document.querySelector(".header-cont");
 
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
 	const url = `/weather?address=${input.value}`;
-
-	weather.innerText = "Loading...";
+	weatherErr.innerHTML = `<img src="./images/loading.svg">`;
 
 	fetch(url)
 		.then((response) => response.json())
 		.then((data) => {
 			if (data.error) {
-				weather.innerHTML = `<p class="error">${data.error}</p>`;
+				weatherErr.innerHTML = `<p class="error">${data.error}</p>`;
 			} else {
-				weather.innerHTML = `<p>Location : ${data.location}</p>
-        <p>Temperature : ${data.temperature}°C</p>
-        <p>Feels Like : ${data.feelslike}°C</p>`;
+				console.log(data);
+				displayCurrent(data.location, data.weatherData.current);
+				displayDaily(data.weatherData.daily);
 			}
 		});
 });
+
+search.addEventListener("click", () => {
+	form.classList.toggle("show-form");
+	header.classList.toggle("mb");
+	search.classList.toggle("close-form");
+});
+
+const displayCurrent = (current) => {};
+
+const displayDaily = (daily) => {};
+
+const convertUNIX = (unix) => {
+	const months = [
+		"Jan",
+		"Feb",
+		"Mar",
+		"Apr",
+		"May",
+		"Jun",
+		"Jul",
+		"Aug",
+		"Sep",
+		"Oct",
+		"Nov",
+		"Dec",
+	];
+	const days = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
+	const date = new Date(unix * 1000);
+	return {
+		dayN: date.getDate(),
+		month: months[date.getMonth()],
+		dayW: days[date.getDay()],
+	};
+};
